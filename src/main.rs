9a6ac8a1f28a4dc6681a00c85b1be978d14100d4
Sky Brewer <jacobrewer@gmail.com>
2021-09-03 18:50:56 +0200

    trying to fix

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ struct Book {
     author: String,
     title: String,
     subjects: Vec<String>,
-    pages: i64,
+    pages: Option<i64>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -55,35 +55,40 @@ async fn import_file(filename: &str) -> Result<Vec<Book>, Box<dyn Error>> {
 }
 
 async fn convert_input(input: &InputBook) -> Result<Book, Box<dyn Error>> {
-    let key = get_book_key(&input.title).await?;
+    let key = get_book_key(input).await?;
     get_book(&key).await
 }
 
 struct Key {
+    title: String,
+    author: String,
     work_key: String,
     edition_key: String,
 }
 
-async fn get_book_key(title: &str) -> Result<Key, Box<dyn Error>> {
-    let converted_title = query_title(title);
+async fn get_book_key(input: &InputBook) -> Result<Key, Box<dyn Error>> {
+    let converted_title = query_title(&input.title);
     let request_url = format!(
         "https://openlibrary.org/search.json?title={title}&fields=key,edition_key&limit=1",
         title = converted_title
     );
 
+    println!("current book is: {:?}", input.title);
     let response = reqwest::get(&request_url).await?;
     let json: Value = response.json().await?;
     let key_value = &json["docs"].as_array().unwrap()[0];
     let work_key = key_value["key"]
         .as_str()
-        .expect(&format!("fail on work_key title: {:?}", title))
+        .expect(&format!("fail on work_key title: {:?}", &input.title))
         .to_string();
     let edition_key = key_value["edition_key"].as_array().unwrap()[0]
         .as_str()
-        .expect(&format!("fail on work_key title: {:?}", title))
+        .expect(&format!("fail on work_key title: {:?}", &input.title))
         .to_string();
 
     Ok(Key {
+        title: input.title.to_string(),
+        author: input.author.to_string(),
         work_key,
         edition_key,
     })
@@ -95,9 +100,6 @@ async fn get_book(key: &Key) -> Result<Book, Box<dyn Error>> {
     let response = reqwest::get(&request_url).await?;
     let json: Value = response.json().await?;
 
-    let title = json["title"].as_str().unwrap().to_string();
-    let author = json["author"].as_str().unwrap().to_string();
-
     let subjects_value = json["subjects"].as_array().unwrap();
     let subjects = subjects_value
         .into_iter()
@@ -106,24 +108,24 @@ async fn get_book(key: &Key) -> Result<Book, Box<dyn Error>> {
 
     let pages = get_pages(&key.edition_key).await?;
 
-    Ok(Book{
-        title,
-        author,
+    Ok(Book {
+        title: key.title.to_string(),
+        author: key.author.to_string(),
         subjects,
-        pages
+        pages,
     })
 }
 
-async fn get_pages(key: &str) -> Result<i64, Box<dyn Error>> {
+async fn get_pages(key: &str) -> Result<Option<i64>, Box<dyn Error>> {
     let request_url = format!("https://openlibrary.org/books/{key}.json?", key = key);
 
     let response = reqwest::get(&request_url).await?;
     let json: Value = response.json().await?;
-    Ok(json["number_of_pages"].as_i64().unwrap())
+    Ok(json["number_of_pages"].as_i64())
 }
 
 fn query_title(title: &str) -> String {
-    title.to_lowercase().replace(" ", "+")
+    title.to_lowercase().replace(" ", "+").replace("'", "")
 }
 
 fn export_file(filename: &str, records: Vec<Book>) -> Result<(), Box<dyn Error>> {
