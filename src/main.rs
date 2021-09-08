bfce8f001b2271e837b7223f158c6fdbc6178faa
Sky Brewer <jacobrewer@gmail.com>
2021-09-08 12:35:11 +0200

    temp file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::{error::Error, fs};
 
 use csv::Writer;
@@ -12,6 +13,8 @@ struct Book {
     title: String,
     subjects: Vec<String>,
     pages: Option<i64>,
+    open_work_key: String,
+    open_edition_key: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -34,23 +37,52 @@ struct InputBook {
 
 #[tokio::main]
 async fn main() {
-    let data = import_file("resources/1001books.csv")
+    let data = process_file("resources/1001books.csv", "resources/temp_output")
         .await
         .expect("fail on read");
     export_file("ouput.csv", data).expect("fail writing");
 }
 
-async fn import_file(filename: &str) -> Result<Vec<Book>, Box<dyn Error>> {
-    let file = fs::read_to_string(filename).expect("couldn't read file");
+fn read_temp_file_keys(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let file = fs::read_to_string(filename).expect("failed to read temp file");
+
     let mut contents = csv::ReaderBuilder::new()
         .has_headers(false)
         .from_reader(file.as_bytes());
+
+    let mut output = vec![];
+    for result in contents.deserialize() {
+        let record: Book = result?;
+        let output_record = record.title;
+        output.push(output_record);
+    }
+    Ok(output)
+}
+
+async fn process_file(
+    filename: &str,
+    temp_output_filename: &str,
+) -> Result<Vec<Book>, Box<dyn Error>> {
+    let file = fs::read_to_string(filename).expect("failed to read file");
+
+    let mut contents = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file.as_bytes());
+
+    let temp_file_keys = read_temp_file_keys(temp_output_filename).expect("get keys ok");
+
+    let mut wtr = Writer::from_path(temp_output_filename)?;
+
     let mut output = vec![];
     for result in contents.deserialize() {
         let record: InputBook = result?;
-
-        output.push(convert_input(&record).await?);
+        if !temp_file_keys.contains(&record.title) {
+            let output_record = convert_input(&record).await?;
+            wtr.serialize(output_record.borrow())?;
+            output.push(output_record);
+        }
     }
+    wtr.flush()?;
     Ok(output)
 }
 
@@ -73,7 +105,7 @@ async fn get_book_key(input: &InputBook) -> Result<Key, Box<dyn Error>> {
         title = converted_title
     );
 
-    println!("current book is: {:?}", input.title);
+    println!("url for getting key: {:?}", request_url);
     let response = reqwest::get(&request_url).await?;
     let json: Value = response.json().await?;
     let key_value = &json["docs"].as_array().unwrap()[0];
@@ -95,16 +127,21 @@ async fn get_book_key(input: &InputBook) -> Result<Key, Box<dyn Error>> {
 }
 
 async fn get_book(key: &Key) -> Result<Book, Box<dyn Error>> {
-    let request_url = format!("https://openlibrary.org/{key}.json", key = key.work_key);
+    let request_url = format!("https://openlibrary.org{key}.json", key = key.work_key);
+    println!("request url; {:?}", request_url);
 
     let response = reqwest::get(&request_url).await?;
     let json: Value = response.json().await?;
 
-    let subjects_value = json["subjects"].as_array().unwrap();
-    let subjects = subjects_value
-        .into_iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>();
+    let subjects_value = json["subjects"].as_array();
+
+    let subjects = match subjects_value {
+        None => vec![],
+        Some(x) => x
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>(),
+    };
 
     let pages = get_pages(&key.edition_key).await?;
 
@@ -113,6 +150,8 @@ async fn get_book(key: &Key) -> Result<Book, Box<dyn Error>> {
         author: key.author.to_string(),
         subjects,
         pages,
+        open_work_key: key.work_key.to_string(),
+        open_edition_key: key.edition_key.to_string(),
     })
 }
 
@@ -125,7 +164,11 @@ async fn get_pages(key: &str) -> Result<Option<i64>, Box<dyn Error>> {
 }
 
 fn query_title(title: &str) -> String {
-    title.to_lowercase().replace(" ", "+").replace("'", "")
+    title
+        .to_lowercase()
+        .replace(" ", "+")
+        .replace("'", "")
+        .replace("’", "")
 }
 
 fn export_file(filename: &str, records: Vec<Book>) -> Result<(), Box<dyn Error>> {
